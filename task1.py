9c4730475c8495a8f96dbe2f3e80fc88f3d6adbf
Degurchaf <120803727+Degurchaf@users.noreply.github.com>
2022-12-18 01:25:29 +0300

    Update task1.py

--- task1.py
+++ task1.py
@@ -30,7 +30,6 @@ class Task:
             return bol
         else: print("Wrong")
 
-print("This is a program for searching for palindromes without taking into account case and some characters.")
 strin = input("Insert string: ")
 t = Task
 print(t.is_palindrome(strin))
